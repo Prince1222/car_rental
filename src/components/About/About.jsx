735a1b97ceb2836cfa8a003806ae90d66b9e1703
Anik Maharjan <maharjanprincetag@gmail.com>
2025-02-05 10:43:13 +0545

    updating

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,10 +10,15 @@ const About = () => {
       <div className='grid grid-cols-1
       sm:grid-cols-2 place-items-center'>
         <div>
-            <img src={CarPng} alt="" />
+            <img src={CarPng} alt=""
+            className='sm:scale-105
+            sm:-translate-x-11 max-h-[300px]
+            drop-shadow-[2px_10px_6px_rgba(0,0,0,0.50)]'
+            
+            />
         </div>
         <div>
-            <div>
+            <div className='space-y-5 sm:p-16 pb-6'>
                 <h1
                 className='text-3xl
                 sm:text-4xl font-bold
@@ -30,6 +35,11 @@ const About = () => {
                     adipisicing elit. Non possimus 
                     eos, id ab unde aspernatur
                 </p>
+                <button
+                className='button-outline'
+                >
+                  Get Started
+                </button>
             </div>
         </div>
       </div>
