cbe4bd11bd01f99b481fc69ac2251dd8b899e239
Anik Maharjan <maharjanprincetag@gmail.com>
2025-02-07 11:55:11 +0545

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,48 +1,119 @@
-import React from 'react'
-import { FaInstagram, FaLinkedin, FaLocationArrow } from 'react-icons/fa'
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
 
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
 const Footer = () => {
   return (
-    <div>
-      <div className="bg-gray-100 dark:bg-dark
-      mt-14 rounded-t-3xl">
-        <div className='grid mf>grid-col-3 py-5'>
-            {/* compnaz  details */}
-            <div className='grid md:grid-cols-3 py-5'>
-              <h1 className='text-3xl font-bold sm:text-left text-justify mb-3'>Car Rental</h1>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam autem repellendus pariatur sint at quo hic earum, temporibus, eos eveniet est laboriosam architecto exercitationem tempore animi incidunt, ratione repellat rem?</p>
-            </div>
+    <div className="bg-gray-100 dark:bg-dark mt-14 rounded-t-3xl">
+      <section className="container">
+        <div className=" grid md:grid-cols-3 py-5">
+          {/* company Details */}
+          <div className=" py-8 px-4 ">
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3 font-serif">
+              Car Rental
+            </h1>
+            <p className="text-sm">
+              Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Possimus, voluptate.{" "}
+            </p>
             <br />
-            <div className='flex items-center
-            gap-3
-            '>
-              <FaLocationArrow/>
-              <p>Balaju,Kathmandu</p>
+            <div className="flex items-center gap-3">
+              <FaLocationArrow />
+              <p>Manamaiju,Kathmandu</p>
             </div>
-            <div className='flex items-center
-            gap-3'>
-            <FaMobileAlt/>
-            <p>+977 9823565656</p>
+            <div className="flex items-center gap-3 mt-3">
+              <FaMobileAlt />
+              <p>+977 97080454578</p>
             </div>
-            {/* social Handles */}
-            <div className='flex items-center  gap-3 mt-6'>
+            {/* Social Handle */}
+            <div className="flex items-center gap-3 mt-6">
               <a href="#">
-                <FaInstagram className='text-3xl 
-                :hover:text-primary duration-300'/>
+                <FaInstagram className="text-3xl hover:text-primary duration-300" />
               </a>
               <a href="#">
-                <FaFacebook className='text-3xl 
-                :hover:text-primary duration-300'/>
+                <FaFacebook className="text-3xl hover:text-primary duration-300" />
               </a>
               <a href="#">
-                <FaLinkedin className='text-3xl 
-                :hover:text-primary duration-300'/>
+                <FaLinkedin className="text-3xl hover:text-primary duration-300" />
               </a>
             </div>
+          </div>
+          {/* Links */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10 ">
+            <div className="">
+              <div className="py-8 px-4 ">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Important Links
+                </h1>
+                <ul className={`flex flex-col gap-3`}>
+                  {FooterLinks.map((link) => (
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-500 dark:text-gray-200">
+                      <span>&#11162;</span>
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              <div className="py-8 px-4 ">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-500 dark:text-gray-200">
+                      <span>&#11162;</span>
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              <div className="py-8 px-4 ">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Location
+                </h1>
+                {/* <ul className="list-disc list-inside"> */}
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-500 dark:text-gray-200">
+                      <span>&#11162;</span>
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
